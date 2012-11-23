5a22ba9b13c554452e83e67d0c9da3eaf5a3bfb0
HugoPoi <hugo.poissonnet@gmail.com>
2012-11-23 15:47:51 +0100

    add a action -f --file

--- arg_treatment.c
+++ arg_treatment.c
@@ -61,23 +61,38 @@ int arg_treatment(int *argc, char **argv, struct arguments *args) {
 	//treatment of argv check number
 	//return std error status
 
-	int pos_pattern, pos_file,i=0;
+	int pos_pattern, pos_file, i = 0;
 
 	for (i = 1; i < *argc; i++) {
 
-		//detect the option --help
+		//option --help
 		if (strcmp(argv[i], "--help") == 0) {
+			args->opt_help = 1;
 			fprintf(stdout,"help of grep\n");
 			exit(2);
 		}
-		if(strcmp(argv[i],"--version")==0) {
+
+		//option --version
+		if (strcmp(argv[i], "--version") == 0 || strcmp(argv[i], "-V") == 0) {
+			args->opt_V = 1;
 			fprintf(stdout,"version of grep\n");
 			exit(2);
 		}
 
+		//option -f=
+		if (strncmp(argv[i], "-f=", 3) == 0) {
+			args->opt_f = 1;
+			args->opt_f_file = &argv[i][3];
+		}
+		//option --file=
+		if (strncmp(argv[i], "--file=", 7) == 0) {
+			args->opt_f = 1;
+			args->opt_f_file = &argv[i][7];
+		}
+
 	}
 
-	if(*argc < 3){
+	if (*argc < 3) {
 		fprintf(stdout,"help of grep\n");
 		exit(2);
 	}
