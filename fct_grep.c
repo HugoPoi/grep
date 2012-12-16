089fdd7e7887101758de74a2a1d817eb9f4961ef
HugoPoi <hugo.poissonnet@gmail.com>
2012-12-16 17:41:59 +0100

    -B -A -C OK (voir sur les gros fichiers) close le #13

--- fct_grep.c
+++ fct_grep.c
@@ -39,7 +39,7 @@ int run_grep_onafile(char *afilepath, struct arguments *args) {
 	//unsigned int count_B = 0;
 	unsigned int line_print_count = 0;
 
-	fifo_savenextline(&before_line_pos,file,args->opt_B_lines); //save the pos before for context
+	fifo_savenextline(&before_line_pos, file, args->opt_B_lines); //save the pos before for context
 	if (args->opt_b)
 		byte_offset = ftell(file);
 	read_error_line =
@@ -54,10 +54,11 @@ int run_grep_onafile(char *afilepath, struct arguments *args) {
 			match_count++;
 			if (!(args->opt_B == 1 || args->opt_C == 1 || args->opt_A == 1)) {
 
-				printline(currentline,line_count,byte_offset,args);
+				printline(currentline, line_count, byte_offset, args);
 
 			} else {
 				//Print with context
+				if(args->opt_B == 1 || args->opt_C == 1){
 				current_byte_offset = ftell(file);
 				fsetpos(file, before_line_pos->data);
 				line_print_count = 0;
@@ -67,20 +68,45 @@ int run_grep_onafile(char *afilepath, struct arguments *args) {
 										&defaultsize) :
 								get_line_file_nd(file, &currentline,
 										&defaultsize);
-				while ((line_print_count
-						<= (args->opt_B_lines))
+				while ((line_print_count <= (args->opt_B_lines))
 						&& !read_error_line) {
 
 					line_print_count++;
 
-					printline(currentline,line_count,byte_offset,args);
-					if(current_byte_offset==ftell(file)) break;
+					printline(currentline, line_count, byte_offset, args);
+					if (current_byte_offset == ftell(file))
+						break;
 					read_error_line =
 							(!args->opt_z) ?
 									get_line_file(file, &currentline,
 											&defaultsize) :
 									get_line_file_nd(file, &currentline,
 											&defaultsize);
+				}
+				}
+				if(args->opt_A == 1) printline(currentline, line_count, byte_offset, args);
+				if(args->opt_C == 1 || args->opt_A == 1){
+				line_print_count = 0;
+				read_error_line =
+						(!args->opt_z) ?
+								get_line_file(file, &currentline,
+										&defaultsize) :
+								get_line_file_nd(file, &currentline,
+										&defaultsize);
+				while ((line_print_count < (args->opt_A_lines))
+						&& !read_error_line) {
+
+					line_print_count++;
+
+					printline(currentline, line_count, byte_offset, args);
+
+					read_error_line =
+							(!args->opt_z) ?
+									get_line_file(file, &currentline,
+											&defaultsize) :
+									get_line_file_nd(file, &currentline,
+											&defaultsize);
+				}
 				}
 				fsetpos(file, &current_endline_pos);
 
@@ -93,7 +119,7 @@ int run_grep_onafile(char *afilepath, struct arguments *args) {
 			byte_offset = ftell(file);
 
 		if (args->opt_B == 1 || args->opt_C == 1) {
-			fifo_savenextline(&before_line_pos,file,args->opt_B_lines);
+			fifo_savenextline(&before_line_pos, file, args->opt_B_lines);
 		}
 
 		read_error_line =
@@ -110,19 +136,19 @@ int run_grep_onafile(char *afilepath, struct arguments *args) {
 	return 0;
 }
 
-int printline(char *aline,unsigned int line_count,long byte_offset,struct arguments *args){
+int printline(char *aline, unsigned int line_count, long byte_offset,
+		struct arguments *args) {
 	char printout_n[100] = "";
 	char printout_b[100] = "";
 	//Simple Print out without context
-					if (args->opt_n) {
-						snprintf(printout_n, 100, "%u:", line_count);
-					}
-					if (args->opt_b) {
-						snprintf(printout_b, 100, "%lu:", byte_offset);
-					}
-					printf("%s%s%s%s%s\n", (args->opt_H) ? args->file_path : "",
-							(args->opt_H) ? ":" : "", printout_n, printout_b,
-							aline);
-					//End Simple Print out without context
+	if (args->opt_n) {
+		snprintf(printout_n, 100, "%u:", line_count);
+	}
+	if (args->opt_b) {
+		snprintf(printout_b, 100, "%lu:", byte_offset);
+	}
+	printf("%s%s%s%s%s\n", (args->opt_H) ? args->file_path : "",
+			(args->opt_H) ? ":" : "", printout_n, printout_b, aline);
+	//End Simple Print out without context
 	return 1;
 }
